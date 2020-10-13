d9ac517b1587c9745b9ad0d61b5d86a24443996e
Utkarsh Jain <utkarshjain@Utkarshs-MacBook-Pro.local>
2020-10-13 21:00:24 +0530

    Intial working code for sqrt

---
+++ sqrt.c
@@ -0,0 +1,22 @@
+/* This program computes the square root of a
+positive number. 
+Name: Utkarsh Jain
+Roll no: B17029
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+	
+	if (argc != 2) {
+		printf("Usage: sqrt input\n\n");
+		exit(-1);
+	}
+
+	int input = atoi(argv[1]);
+	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	return(0);
+
+} // end main
