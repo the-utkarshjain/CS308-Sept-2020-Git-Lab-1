277aea01b8c8d75a5fd97873ad905f5270ab6aa0
Utkarsh Jain <utkarshjain@Utkarshs-MacBook-Pro.local>
2020-10-13 21:18:32 +0530

    Added exit message

--- sqrt.c
+++ sqrt.c
@@ -19,5 +19,6 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	return(0);
-
+	
+	printf("End of program. Exiting.");
 } // end main
