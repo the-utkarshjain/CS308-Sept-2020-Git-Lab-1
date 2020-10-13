afafd138089cbcc4f1dd604018741c5d4973663e
Utkarsh Jain <utkarshjain@Utkarshs-MacBook-Pro.local>
2020-10-13 21:43:10 +0530

    Added support for complex number

--- sqrt.c
+++ sqrt.c
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]) {
 
 	int input = atoi(argv[1]);
 	if(input < 0) {
-		printf("Input is invalid");
-		exit(-1);
+		printf("Sqrt of %d is %f i\n",input, sqrt(abs(input)));
+		return(0);
 	}
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	return(0);
